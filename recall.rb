929531f58adeb22d4d10b12b34fcd75ab79aa767
StikyPiston <156162907+StikyPiston@users.noreply.github.com>
2025-12-15 12:00:08 +0200

    Fix the url being in the wrong place

--- recall.rb
+++ recall.rb
@@ -1,7 +1,7 @@
 class Recall < Formula
   desc "A simple to-do list for the terminal. Written in Swift."
-  homepage "https://github.com/StikyPiston/recall/archive/refs/tags/151225a.zip"
-  url ""
+  url "https://github.com/StikyPiston/recall/archive/refs/tags/151225a.zip"
+  homepage ""
   sha256 "797c84b77b82184cd3a1c43d6b3c5fd5a451448063b9fe716fff0ae486a77de"
   version "151225a"
   license "MIT"
