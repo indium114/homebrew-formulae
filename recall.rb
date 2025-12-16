c28b667f8f7d6ec6e68ef6ead3f721898be11525
StikyPiston <156162907+StikyPiston@users.noreply.github.com>
2025-12-16 10:55:47 +0200

    Update recall to 161225a

--- recall.rb
+++ recall.rb
@@ -1,9 +1,9 @@
 class Recall < Formula
   desc "A simple to-do list for the terminal. Written in Swift."
-  url "https://github.com/StikyPiston/recall/archive/refs/tags/151225a.zip"
+  url "https://github.com/StikyPiston/recall/archive/refs/tags/161225a.zip"
   homepage ""
-  sha256 "797c84b77b82184cd3a1c43d6b3c5fd5a451448063b9fe716fff0ae486a77de7"
-  version "151225a"
+  sha256 "8fcb744c8554078c80e42c3671dd08ce2b7bbcc8fd06c19c729ea57d3ba78ac7"
+  version "161225a"
   license "MIT"
   
   depends_on "swift" => :build
