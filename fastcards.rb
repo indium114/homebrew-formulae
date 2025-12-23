223f1f817a36d1bfc673b9aa11de68023a0f81e1
StikyPiston <156162907+StikyPiston@users.noreply.github.com>
2025-12-23 11:27:06 +0200

    Fix sha256sum for fastcards

--- fastcards.rb
+++ fastcards.rb
@@ -2,7 +2,7 @@ class Fastcards < Formula
   desc "A CLI-based spaced repetition flashcard study tool"
   homepage ""
   url "https://github.com/StikyPiston/fastcards/archive/refs/tags/2025.12.23-a.zip"
-  sha256 "cc81377426fb40ce0df652cd0ba2e738c4297ca6773b1b8721d9250e43765630"
+  sha256 "6391912c13aa7a9048a542681e98b4fc3a46097a3c3760359944ddb5e34e72c0"
   version "2025.12.23-a"
   license "MIT"
   
