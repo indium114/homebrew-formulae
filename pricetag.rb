52fcdb7c8801601d2a49384ac69d7a44deae4697
StikyPiston <156162907+StikyPiston@users.noreply.github.com>
2025-12-25 20:23:53 +0200

    Update pricetag to 2025.12.25-c

--- pricetag.rb
+++ pricetag.rb
@@ -1,9 +1,9 @@
 class Pricetag < Formula
   desc "A CLI-based file tagging tool"
   homepage ""
-  url "https://github.com/StikyPiston/pricetag/archive/refs/tags/2025.12.25-a.zip"
-  sha256 "b496cbbc15f0cebe12e5d3ef323ca96041b6e06086661b03c2fdf34e0de5e4b9"
-  version "2025.12.25-a"
+  url "https://github.com/StikyPiston/pricetag/archive/refs/tags/2025.12.25-c.zip"
+  sha256 "8e7c0859e5e9d2a828d0aa79a47e4ee107094d0d043f132f8f492c6f15693bee"
+  version "2025.12.25-c"
   license "MIT"
   
   depends_on "swift" => :build
