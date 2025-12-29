1d051d513e51125e9ad877fe943cda22dbaed297
StikyPiston <156162907+StikyPiston@users.noreply.github.com>
2025-12-29 10:22:03 +0200

    Go back to installing binary for chess-tui

--- chess-tui.rb
+++ chess-tui.rb
@@ -1,17 +1,13 @@
 class ChessTui < Formula
   desc "Play chess from your terminal"
   homepage "https://thomas-mauran.github.io/chess-tui/"
-  url "https://github.com/thomas-mauran/chess-tui/archive/refs/tags/2.2.0.zip"
+  url "https://github.com/thomas-mauran/chess-tui/releases/download/2.2.0/chess-tui-2.2.0-x86_64-unknown-linux-gnu.tar.gz"
   sha256 "c1942e4ee40ca96e30275672e3b6dc1c56e507e44fbb60cfea9c4d8fa5a05d9a"
   version "2.2.0"
   license "MIT"
 
-  depends_on "rustup" => :build
-
   def install
-    system "rustup default nightly"
-    system "cargo build --release"
-    bin.install "target/release/chess-tui"
+    bin.install "chess-tui"
   end
 
   test do
