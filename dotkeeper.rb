9cc19239bde976f4de02da16a8e1690b493125c9
StikyPiston <156162907+StikyPiston@users.noreply.github.com>
2026-01-13 16:13:38 +0200

    Update dotkeeper formula to comply with audit

--- dotkeeper.rb
+++ dotkeeper.rb
@@ -1,6 +1,6 @@
 class Dotkeeper < Formula
   desc "A dotfile management/symlink farm tool. Successor to rancher."
-  homepage ""
+  homepage "https://github.com/stikypiston/dotkeeper"
   url "https://github.com/StikyPiston/dotkeeper/archive/refs/tags/2025.12.19-a.zip"
   sha256 "b84d19d95137f5d35d7471968b7fceba65bf89bc9798b150c86dd8fdc776db0f"
   version "2025.12.19-a"
@@ -9,11 +9,11 @@ class Dotkeeper < Formula
   depends_on "swift" => :build
 
   def install
-    system "swift build --configuration release"
+    system "swift" "build" "--configuration" "release"
     bin.install ".build/release/dotkeeper"
   end
 
   test do
-    system "dotkeeper"
+    system "#{bin}/dotkeeper"
   end
 end
