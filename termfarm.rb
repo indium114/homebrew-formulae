83f9c4e85f84cee3a99c65fe8876f3bc116b8e23
StikyPiston <156162907+StikyPiston@users.noreply.github.com>
2026-01-15 17:12:48 +0200

    Update termfarm to 2026.01.15-a

--- termfarm.rb
+++ termfarm.rb
@@ -1,9 +1,9 @@
 class Termfarm < Formula
   desc "CLI idle farming game"
   homepage "https://github.com/stikypiston/termfarm"
-  url "https://github.com/StikyPiston/termfarm/archive/refs/tags/2026.01.14-b.tar.gz"
-  version "2026.01.14-b"
-  sha256 "f5586dfadcc4c548a86b227940430da653dd3ded39d7c415350ebdbc47feac36"
+  url "https://github.com/StikyPiston/termfarm/archive/refs/tags/2026.01.15-a.tar.gz"
+  version "2026.01.14-a"
+  sha256 "5660dfd3af5ac8040d18b4feb45439ef1d91113a31ec7e1040af8dbebd207a0c"
   license "MIT"
 
   depends_on "swift" => :build
