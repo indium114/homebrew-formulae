0f4d101e8386a2d519f8de5d6294239044183485
StikyPiston <156162907+StikyPiston@users.noreply.github.com>
2026-01-15 17:15:10 +0200

    Fix the version number for termfarm

--- termfarm.rb
+++ termfarm.rb
@@ -2,7 +2,7 @@ class Termfarm < Formula
   desc "CLI idle farming game"
   homepage "https://github.com/stikypiston/termfarm"
   url "https://github.com/StikyPiston/termfarm/archive/refs/tags/2026.01.15-a.tar.gz"
-  version "2026.01.14-a"
+  version "2026.01.15-a"
   sha256 "5660dfd3af5ac8040d18b4feb45439ef1d91113a31ec7e1040af8dbebd207a0c"
   license "MIT"
 
