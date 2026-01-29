7102935a5a35bc519a7990b023423643f467528a
StikyPiston <156162907+StikyPiston@users.noreply.github.com>
2026-01-29 20:20:54 +0200

    Create versioned formula for dotkeeper 2025.12.29-a

---
+++ dotkeeper@2025.12.19-a.rb
@@ -0,0 +1,19 @@
+class DotkeeperAT2025.12.29_a < Formula
+  desc "Dotfile management/symlink farm tool"
+  homepage "https://github.com/stikypiston/dotkeeper"
+  url "https://github.com/StikyPiston/dotkeeper/archive/refs/tags/2025.12.19-a.tar.gz"
+  version "2025.12.19-a"
+  sha256 "d25ddc2f7e211f648300a4a4a257495a237d3eb0a76df946c54c83f9d76826b8"
+  license "MIT"
+
+  depends_on "swift" => :build
+
+  def install
+    system "swift", "build", "--configuration", "release", "-Xswiftc", "-static-stdlib"
+    bin.install ".build/release/dotkeeper"
+  end
+
+  test do
+    system bin/"dotkeeper"
+  end
+end
