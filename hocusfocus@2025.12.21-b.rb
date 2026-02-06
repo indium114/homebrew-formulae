16a9a1784f5d541af336d138f10d90034b34b478
StikyPiston <156162907+StikyPiston@users.noreply.github.com>
2026-02-06 20:33:12 +0200

    Versioned formula for hocusfocus 2025.12.21-b

---
+++ hocusfocus@2025.12.21-b.rb
@@ -0,0 +1,19 @@
+class HocusfocusAT20251221A < Formula
+  desc "A simple productivity-tracking tool"
+  homepage ""
+  url "https://github.com/StikyPiston/hocusfocus/archive/refs/tags/2025.12.21-b.zip"
+  sha256 "cc81377426fb40ce0df652cd0ba2e738c4297ca6773b1b8721d9250e43765630"
+  version "2025.12.21-b"
+  license "MIT"
+  
+  depends_on "swift" => :build
+
+  def install
+    system "swift build --configuration release"
+    bin.install ".build/release/hocusfocus"
+  end
+
+  test do
+    system "hocusfocus"
+  end
+end
