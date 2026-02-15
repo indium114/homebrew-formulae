a3368b12facffaec80b9a4109ae30afc1236d185
StikyPiston <156162907+StikyPiston@users.noreply.github.com>
2026-02-15 15:54:39 +0200

    Versioned formula for fastcards 2025.12.23-a

---
+++ fastcards@2025.12.23-a.rb
@@ -0,0 +1,19 @@
+class FastcardsAT20251223A < Formula
+  desc "A CLI-based spaced repetition flashcard study tool"
+  homepage ""
+  url "https://github.com/StikyPiston/fastcards/archive/refs/tags/2025.12.23-a.zip"
+  sha256 "6391912c13aa7a9048a542681e98b4fc3a46097a3c3760359944ddb5e34e72c0"
+  version "2025.12.23-a"
+  license "MIT"
+
+  depends_on "swift" => :build
+
+  def install
+    system "swift build --configuration release"
+    bin.install ".build/release/fastcards"
+  end
+
+  test do
+    system "fastcards amount"
+  end
+end
