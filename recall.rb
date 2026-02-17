60bd5b24b7c12141f6d83a7f288ab0d1a1b2c11d
StikyPiston <156162907+StikyPiston@users.noreply.github.com>
2026-02-17 17:32:04 +0200

    Update recall to 2026.02.17-a

--- recall.rb
+++ recall.rb
@@ -1,11 +1,11 @@
 class Recall < Formula
   desc "A simple to-do list for the terminal. Written in Swift."
-  url "https://github.com/StikyPiston/recall/archive/refs/tags/2026.01.30-a.zip"
+  url "https://github.com/StikyPiston/recall/archive/refs/tags/2026.02.17-a.zip"
   homepage ""
-  sha256 "333de942af4267b59b3bb7f420566d22ee970c8c42e36768548f2b4249606e6f"
-  version "2026.01.30-a"
+  sha256 "a1b9cb74a3727341bb8adccb8f6e7900f5845280405a083bfe292b1ed5057e45"
+  version "2026.02.17-a"
   license "MIT"
-  
+
   depends_on "go" => :build
 
   def install
