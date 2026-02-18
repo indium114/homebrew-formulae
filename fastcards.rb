79112e24283c02f48d6509d12270db4452a5d97f
StikyPiston <156162907+StikyPiston@users.noreply.github.com>
2026-02-18 20:07:22 +0200

    Update fastcards to 2026.02.18-a

--- fastcards.rb
+++ fastcards.rb
@@ -1,9 +1,9 @@
 class Fastcards < Formula
   desc "A CLI-based spaced repetition flashcard study tool"
   homepage ""
-  url "https://github.com/StikyPiston/fastcards/archive/refs/tags/2026.02.15-a.zip"
-  sha256 "cd3361dbfd438dcd399d46810ab4782eca23f697b97380c2b3a6f14ba8c0510a"
-  version "2026.02.15-a"
+  url "https://github.com/StikyPiston/fastcards/archive/refs/tags/2026.02.18-a.zip"
+  sha256 "f794b87a6ea47ef43eb3a0a3a6a989d2b28620fc0376d9a814eaf7384de84d9f"
+  version "2026.02.18-a"
   license "MIT"
 
   depends_on "go" => :build
