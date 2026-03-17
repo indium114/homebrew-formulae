596773e5b60a45385106338e7805eb6c17d7939d
StikyPiston <156162907+StikyPiston@users.noreply.github.com>
2026-03-17 12:02:16 +0200

    Update dotkeeper to 2026.03.17-a

--- dotkeeper.rb
+++ dotkeeper.rb
@@ -1,9 +1,9 @@
 class Dotkeeper < Formula
   desc "Dotfile management/symlink farm tool"
   homepage "https://github.com/stikypiston/dotkeeper"
-  url "https://github.com/StikyPiston/dotkeeper/archive/refs/tags/2026.03.14-a.tar.gz"
-  version "2026.03.14-a"
-  sha256 "dfa6db027175d47b721a818d7e3654c2a1f6dfb2f9a60a16f0956c3ff8ab24f0"
+  url "https://github.com/StikyPiston/dotkeeper/archive/refs/tags/2026.03.17-a.tar.gz"
+  version "2026.03.17-a"
+  sha256 "7691349a6e97c24d2578acc865ed1afd7de6eb9465554e780a89c58c73dc8d2a"
   license "MIT"
 
   depends_on "go" => :build
