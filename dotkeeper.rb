4b4f9770a3835aaa72c0addb174d1f553c55f0d5
StikyPiston <156162907+StikyPiston@users.noreply.github.com>
2026-03-17 12:39:04 +0200

    Fix checksum in dotkeeper

--- dotkeeper.rb
+++ dotkeeper.rb
@@ -3,7 +3,7 @@ class Dotkeeper < Formula
   homepage "https://github.com/stikypiston/dotkeeper"
   url "https://github.com/StikyPiston/dotkeeper/archive/refs/tags/2026.03.17-b.tar.gz"
   version "2026.03.17-b"
-  sha256 "0ed36d22d74b1293bdcc9e9f3fa31be2d5cfd9c9cbf774f818f6a354c8567e7a"
+  sha256 "b930955aab5a66d51c12f53f1f3dc87165d163e69c14574695d2571e09178ff3"
   license "MIT"
 
   depends_on "go" => :build
